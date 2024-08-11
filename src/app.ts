090fbfa6b3f3119c0823c5c21ae1415592b9a5cd
aimee rivers <aimee@aimeerivers.com>
2024-08-11 09:31:58 +0200

    feat(auth): enable logging in and out, directing to user-service (#4)
    * feat(auth): enable logging in and out, directing to user-service
    
    with callbackUrl so user-service knows where to send you back
    
    * refactor(auth): rename authentication to auth

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,12 @@ import dotenv from "dotenv";
 import express from "express";
 import path from "path";
 
-import { findUserFromSession, RequestWithUser } from "./authentication";
+import { findUserFromSession, RequestWithUser } from "./auth";
 
 dotenv.config();
+
+const baseUrl = process.env.BASE_URL ?? "http://localhost:7283";
+const userServiceUrl = process.env.USER_SERVICE_URL ?? "http://localhost:8583";
 
 const app = express();
 
@@ -18,7 +21,7 @@ app.set("views", path.join(appRootPath, "views"));
 app.use(express.static(path.join(appRootPath, "public")));
 
 app.get("/", findUserFromSession, (req: RequestWithUser, res) => {
-  res.render("home-page", { user: req.user });
+  res.render("home-page", { user: req.user, userServiceUrl, callbackUrl: new URL(req.url, baseUrl).toString() });
 });
 
 export { app };
