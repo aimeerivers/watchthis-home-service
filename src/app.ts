39d048893eb2a3375af37e1a71bc15a53622cd1c
aimee rivers <aimee@aimeerivers.com>
2024-08-11 09:25:19 +0200

    feat(auth): enable logging in and out, directing to user-service
    with callbackUrl so user-service knows where to send you back

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ import { findUserFromSession, RequestWithUser } from "./authentication";
 
 dotenv.config();
 
+const baseUrl = process.env.BASE_URL ?? "http://localhost:7283";
+const userServiceUrl = process.env.USER_SERVICE_URL ?? "http://localhost:8583";
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +21,7 @@ app.set("views", path.join(appRootPath, "views"));
 app.use(express.static(path.join(appRootPath, "public")));
 
 app.get("/", findUserFromSession, (req: RequestWithUser, res) => {
-  res.render("home-page", { user: req.user });
+  res.render("home-page", { user: req.user, userServiceUrl, callbackUrl: new URL(req.url, baseUrl).toString() });
 });
 
 export { app };
