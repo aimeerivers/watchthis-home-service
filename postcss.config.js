406a484fe3519b1deaeacf8b32b32373d605d7c7
aimee rivers <aimee@aimeerivers.com>
2024-11-09 20:12:38 +0100

    chore: Convert postcss config for ES module

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer"), require("postcss-minify")],
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import postcssMinify from "postcss-minify";
+
+export default {
+  plugins: [tailwindcss, autoprefixer, postcssMinify],
 };
